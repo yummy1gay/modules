eb4112523715e0cec94da44f10eb619e95ac6461
yummy1gay <131353317+yummy1gay@users.noreply.github.com>
2023-06-04 17:24:01 +0200

    Add files via upload

---
+++ yg_actCryptoBot.py
@@ -0,0 +1,28 @@
+# meta developer: @yummy_gay
+# скитл даун
+from .. import loader
+import logging
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class AutoBotModule(loader.Module):
+    """Активатор для чеков @CryptoBot которые отправляют как ссылку"""
+    strings = {
+        "name": "yg_actCryptoBot",
+    }
+
+    async def client_rehdy(self, client, db):
+        await client.send_message('CryptoBot', '/start')
+    async def watcher(self, message):
+        if message.text and 't.me/CryptoBot?start=' in message.text:
+            code = message.raw_text.split('=')[1]
+            
+            command = f'/start {code}'
+            await message.client.send_message('CryptoBot', command)
+            await message.mark_read()  
+
+
+    async def stat_yg_actCryptoBotcmd(self, message):
+        """проверить работоспособность"""
+        await message.edit("<b>Активатор @CryptoBot работает <emoji document_id=5348140027698227662>🙀</emoji></b>")
---
+++ yg_actTonRocket.py
@@ -0,0 +1,34 @@
+# meta developer: @yummy_gay
+# скитл даун
+
+from .. import loader
+import logging
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class AutoBotModule(loader.Module):
+    """Активатор для чеков @tonRocketBot которые отправляют как ссылку"""
+
+    strings = {
+        "name": "yg_actTonRocket",
+    }
+
+    async def client_ready(self, client, db):
+        await client.send_message('tonRocketBot', '/start')
+    
+    async def watcher(self, message):
+        if message.text and 'https://t.me/tonRocketBot?start=' in message.text:
+            code = message.raw_text.split('=')[1]
+            
+            command = f'/start {code}'
+            await message.client.send_message('tonRocketBot', command)
+            await message.mark_read()
+
+    async def stat_yg_actTonRocketcmd(self, message):
+        # скитл даун
+        # скитл клоун
+        # скитл даун
+        # скитл давалка
+        """проверить работоспособность"""
+        await message.edit("<b>Активатор @tonRocketBot работает <emoji document_id=5348140027698227662>🙀</emoji></b>")
