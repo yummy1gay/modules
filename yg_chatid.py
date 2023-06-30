c64ef6756e877b1c58979b08c1217a89c511206b
yummy1gay <131353317+yummy1gay@users.noreply.github.com>
2023-06-30 18:53:17 +0300

    Add files via upload

--- yg_chatid.py
+++ yg_chatid.py
@@ -8,10 +8,10 @@ class yg_CHATIDModule(loader.Module):
 
     strings = {
         "name": "yg_chatid",
-        "group_id": "<b>Group ID:</b> <code>{group_id}</code>"
+        "group_id": "<b>Chat ID:</b> <code>{group_id}</code>"
     }
 
-    async def groupidcmd(self, message):
+    async def chatidcmd(self, message):
         """отображает ID чата"""
         group_id = message.chat_id
         await message.edit(self.strings["group_id"].format(group_id=group_id))
