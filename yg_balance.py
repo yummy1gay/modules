36c27c3e4abaf2eafa8f8bf2e2ea1b9a91af8d63
yummy1gay <131353317+yummy1gay@users.noreply.github.com>
2025-02-25 02:15:51 +0100

    Add files via upload

--- yg_balance.py
+++ yg_balance.py
@@ -1,4 +1,4 @@
-__version__ = (1, 4, 8, 8)
+__version__ = (1, 5)
 
 # This file is a part of Hikka Userbot
 # Code is NOT licensed under CC-BY-NC-ND 4.0 unless otherwise specified.
@@ -14,14 +14,30 @@ __version__ = (1, 4, 8, 8)
 # █▄█ █░█ █▀▄▀█ █▀▄▀█ █▄█   █▀▄▀█ █▀█ █▀▄ █▀
 # ░█░ █▄█ █░▀░█ █░▀░█ ░█░   █░▀░█ █▄█ █▄▀ ▄█
 
+import re
+import cloudscraper
+import urllib.parse, json
 from telethon import TelegramClient
+from telethon.tl.functions.messages import RequestAppWebViewRequest
+from telethon.tl.types import InputBotAppShortName
 
 from .. import loader, utils
 
+@loader.tds
 class yg_balance(loader.Module):
     """Модуль для просмотра балансов в @CryptoBot и @wallet"""
 
     strings = {
+        "name": "yg_balance",
+        "cryptobot": (
+            "<emoji document_id=5217705010539812022>☺️</emoji> <b>My balance in @CryptoBot:</b>\n\n{}"
+        ),
+        "wallet": (
+            "<emoji document_id=5438394062434485433>💎</emoji> <b>My balance in @wallet:</b>\n\n{}"
+        ),
+    }
+
+    strings_ru = {
         "name": "yg_balance",
         "cryptobot": (
             "<emoji document_id=5217705010539812022>☺️</emoji> <b>Мой баланс в @CryptoBot:</b>\n\n{}"
@@ -31,16 +47,31 @@ class yg_balance(loader.Module):
         ),
     }
 
-    emoji_map = {
-        "Bitcoin": "<emoji document_id=5215590800003451651>🪙</emoji>",
-        "Toncoin": "<emoji document_id=5215276644620586569>🪙</emoji>",
-        "USDT": "<emoji document_id=5215699136258524363>🪙</emoji>",
-        "NOT": "<emoji document_id=5215699170618263097>🪙</emoji>",
-        "DOGS": "<emoji document_id=5341347975006280049>🪙</emoji>"
+    strings_ua = {
+        "name": "yg_balance",
+        "cryptobot": (
+            "<emoji document_id=5217705010539812022>☺️</emoji> <b>Мій баланс у @CryptoBot:</b>\n\n{}"
+        ),
+        "wallet": (
+            "<emoji document_id=5438394062434485433>💎</emoji> <b>Мій баланс у @wallet:</b>\n\n{}"
+        ),
     }
 
-    @staticmethod
-    async def check(bot, message):
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "hide_0_balances",
+                True,
+                "hiding zero balances",
+                validator=loader.validators.Boolean()
+            )
+        )
+
+    async def client_ready(self, client: TelegramClient, db):
+        self.client = client
+        self.scraper = cloudscraper.create_scraper()
+
+    async def check(self, bot, message):
         async with message.client.conversation(bot) as conv:
             request = await conv.send_message("/wallet")
             answer = await conv.get_response()
@@ -50,36 +81,92 @@ class yg_balance(loader.Module):
 
         return answer.text
 
-    @staticmethod
-    def cryptobot(text):
-        lines = [line.strip() for line in text.split('\n') if line.strip() and not 'Кошелёк' in line]
-        balance_lines = [line for line in lines if not line.startswith('≈')]
+    def cryptobot(self, text):
+        hide_zeros = self.config["hide_0_balances"]
+        
+        lines = [line.strip() for line in text.split('\n') if line.strip() and 'Кошелёк' not in line]
         total_line = next((line for line in lines if line.startswith('≈')), '')
+        balance_lines = [line for line in lines if not line.startswith('≈')]
+
+        if hide_zeros:
+            balance_lines = [line for line in balance_lines if not re.search(r':\s?0(?:\.0+)?\s\w+', line)]
+
         return '\n'.join(balance_lines) + (f"\n\n{total_line}" if total_line else '')
 
-    @staticmethod
-    def wallet(text):
-        lines = [line.strip() for line in text.split('\n') if line.strip() and not 'Мой кошелёк' in line]
-        formatted_lines = []
-        for line in lines:
-            for crypto, emoji in yg_balance.emoji_map.items():
-                if crypto in line:
-                    formatted_lines.append(f"{emoji} {line}")
-                    break
-            else:
-                formatted_lines.append(line)
-        return '\n'.join(formatted_lines)
+    async def auth(self):
+        bot = await self.client.get_input_entity(1985737506)
+        app = InputBotAppShortName(bot_id=bot, short_name="start")
 
-    @loader.command()
+        web_view = await self.client(RequestAppWebViewRequest(
+            peer='me',
+            app=app,
+            platform='android'
+        ))
+
+        return web_view.url
+    
+    async def login(self):
+        url = await self.auth()
+        fragment = urllib.parse.unquote(urllib.parse.unquote(urllib.parse.urlparse(url).fragment[13:]))
+        params = dict(urllib.parse.parse_qsl(fragment))
+        params["user"] = json.loads(params.get("user", "{}"))
+
+        data = {
+            **params,
+            "web_view_init_data_raw": urllib.parse.unquote(url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
+        }
+
+        resp = self.scraper.post(
+            f"https://walletbot.me/api/v1/users/auth/",
+            json=data
+        )
+
+        return resp.json().get('value', {})
+    
+    async def get_balances(self):
+        resp = self.scraper.get(
+            "https://walletbot.me/api/v1/accounts/",
+            headers={
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Accept-Language": "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+                "Authorization": await self.login(),
+            },
+        )
+
+        hide_zeros = self.config["hide_0_balances"]
+        balances = [
+            f"<b>{account['currency']}:</b> {account['available_balance']} ({account['available_balance_usd_amount']}$)"
+            for account in resp.json().get("accounts", [])
+            if not (hide_zeros and account['available_balance'] == 0 and account['available_balance_usd_amount'] == 0.0)
+        ]
+        
+        return balances
+    
+    @loader.command(ru_doc="проверить баланс в @CryptoBot",
+                    ua_doc="перевірити баланс у @CryptoBot")
     async def bc(self, message):
-        """проверить баланс в @CryptoBot"""
+        """check balance in @CryptoBot"""
         text = await self.check(1559501630, message)
         balance = self.cryptobot(text)
         await utils.answer(message, self.strings["cryptobot"].format(balance))
 
-    @loader.command()
+    @loader.command(ru_doc="проверить баланс в @wallet",
+                    ua_doc="перевірити баланс у @wallet")
     async def bw(self, message):
-        """проверить баланс в @wallet"""
-        text = await self.check(1985737506, message)
-        balance = self.wallet(text)
+        """check balance in @wallet"""
+        balances = await self.get_balances()
+        balance = "\n".join(balances) if balances else "-"
         await utils.answer(message, self.strings["wallet"].format(balance))
+
+    @loader.command(ru_doc="вкл/выкл скрытие нулевых балансов",
+                    ua_doc="увімк/вимк приховування нульових балансів")
+    async def hide0(self, message):
+        "on/off hiding zero balances"
+        self.config["hide_0_balances"] = not self.config["hide_0_balances"]
+
+        status = 'включено' if self.config["hide_0_balances"] else 'выключено'
+        await utils.answer(
+            message,
+            f"<emoji document_id=5278611606756942667>❤️</emoji> <b>Скрытие нулевых балансов {status}</b>"
+        )
